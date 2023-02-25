176744d9300704726d742f78fbc69635ab9fb1fa
luciotappi <lucio.tappi@qm-equipment.com>
2023-02-25 18:19:51 -0300

    156. Form and input for send/receive

---
+++ public/js/chat.js
@@ -0,0 +1,33 @@
+const socket = io()
+
+// socket.on('countUpdated',(count) => {
+//     console.log('The count has been updated!.Now is ',count)
+// })
+
+// document.querySelector('#increment').addEventListener('click',()=>{
+//     console.log('clicked')
+//     socket.emit('increment')
+// })
+
+
+const messageForm = document.querySelector('#message-form')
+const messageInput = document.querySelector('input')
+
+messageForm.addEventListener('submit',(e) => {
+
+    e.preventDefault()
+    
+    const message = e.target.elements.message.value
+   
+    socket.emit('sendMessage',message)
+
+})
+
+
+socket.on('sendMessage',(message) => {
+    console.log(message)
+})
+
+socket.on('messageConfirmation',(message) => {
+    console.log(message)
+})
--- src/index.js
+++ src/index.js
@@ -1,14 +1,44 @@
 const path = require('path')
+const http = require('http')
 const express = require('express')
+const socketio = require('socket.io')
 
 const app = express()
+const server= http.createServer(app)
+const io = socketio(server)
 
 const port = process.env.PORT || 3000
 const publicDirectoryPath = path.join(__dirname,'../public')
 
 app.use(express.static(publicDirectoryPath))
 
-app.listen(port,()=> {
+
+let count = 0 
+const message= "Welcome!"
+
+io.on('connection',(socket)=> {
+    console.log('New WebSocket connection')
+
+    // socket.emit('countUpdated',count)
+
+    // socket.on('increment',() =>{
+    //     count++
+    //     // socket.emit('countUpdated',count)  // sends answer to One client( the one who asked)
+    //     io.emit('countUpdated',count) // broadcasts the answers to all clientes
+    // })
+
+    socket.emit('message',message)
+
+    socket.on('sendMessage',(message) => {
+        socket.emit('messageConfirmation',"Recepcion OK")
+        io.emit('sendMessage',message)
+    })
+
+})
+
+
+
+server.listen(port,()=> {
 
     console.log(`Server is up on port  ${port}!`)
 })
