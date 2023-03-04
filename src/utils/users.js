b5813d1e2638bc3ee0de5c7d7d1920395434526e
luciotappi <lucio.tappi@qm-equipment.com>
2023-03-04 12:39:14 -0300

    Ready for deploy V1.0.0

---
+++ src/utils/users.js
@@ -0,0 +1,78 @@
+const  users = []
+
+//addUser, removeUser, getUser,getUsersInRoom
+
+const addUser = ({id,username,room}) => {
+
+    // clean the data
+    username = username.trim().toLowerCase()
+    room = room.trim().toLowerCase()
+
+    // validate the data
+
+    if (!username || !room) {
+        return {
+            error: 'USername and room are required'
+        }
+    }
+
+    //check for existing user
+
+    const existingUser = users.find((user) => {
+
+        return user.room === room && user.username === username
+    })
+
+    // Validate username
+
+    if (existingUser) {
+        return {
+            error:'Username is in use!'
+        }
+    }
+
+    // Store user
+
+    const user = {id,username,room}
+    users.push(user)
+    return {user}
+}
+
+const removeUser = ((id)=> {
+
+    const index = users.findIndex((user) => user.id === id )
+
+    if (index !== -1) {
+        return users.splice(index,1)[0]
+    }
+
+})
+
+const getUser = (id) => {
+
+    return users.find((user)=> user.id===id)
+
+    // const findedUsers = users.find((user)=>user.id === id)
+    // // console.log(findedUsers)
+    // if (findedUsers == undefined) {
+    //     return {
+    //         error:' no users finded'
+    //     }
+    // } else
+    // {
+    //     return findedUsers
+    // }
+}
+
+const getUsersInRoom = (room) => {
+    room =room.trim().toLowerCase()
+    return users.filter((user) => user.room === room)
+}
+ 
+module.exports = {
+
+    addUser,
+    removeUser,
+    getUser,
+    getUsersInRoom
+}
